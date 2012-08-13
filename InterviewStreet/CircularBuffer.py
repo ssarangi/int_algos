d1d9ce52b167ba87bf022b0988bcfc5233db9039
unknown <satyajit@BDCL7-SATYAJIT.amd.com>
2012-08-13 14:47:36 -0400

    CircularBuffer.py

---
+++ InterviewStreet/CircularBuffer.py
@@ -0,0 +1,22 @@
+class Buffer:
+    def __init__(self, N):
+        self.max_size = N
+        self.m_list   = []
+        self.pointer  = 0
+    
+    # Here line is a single element
+    def __append__(element):
+        if (pointer == N):
+            pointer = 0
+ 
+        if (pointer > len(self.m_list)):
+            self.m_list.append(element)
+        else:
+            self.m_list[pointer] = element
+            pointer += 1
+
+    # Lines is assumed to be a list
+    def append(element_list):
+        if ():
+
+
